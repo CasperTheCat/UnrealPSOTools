9671b09d5dc9a43e0c249d8c498a897114c091f3
CasperTheCat <zsmyna@gmail.com>
2022-05-19 22:03:44 +1200

    Adding new content

--- tests/Pull.py
+++ tests/Pull.py
@@ -5,7 +5,8 @@ import secrets
 import base64
 import json
 
-uploadURL = "/api/pco/date/after/p/{}/m/{}"
+uploadURL = "/api/pco/date/after/"
+uploadURL = "/api/pco/version/after"
 
 machineID = secrets.token_hex(16)
 version = "0.0.1.1"
@@ -18,17 +19,24 @@ uploadRun = uploadURL.format(projid, machineID)
 header = {"Content-type": "application/json"} 
 
 requestData = {
-    "date": "2000-01-01"
+    "date": "2000-01-01",
+    "machine": machineID,
+    "project": projid,
+    "version": version,
+    "platform": "DirectX",
+    "shadermodel": "SM5"
 }
 
-p = requests.post("http://127.0.0.1:3000" + uploadRun, data=json.dumps(requestData), headers=header)
+p = requests.post("http://127.0.0.1:3000" + uploadURL, data=json.dumps(requestData), headers=header)
 
 print(p.status_code)
 
 if p.status_code == 200:
     jsonblob = p.json()
+    print(len(jsonblob))
     index = 0
     for shader in jsonblob:
+        print("{} -> V{}.{}.{}.{}".format(index, shader["versionmajor"], shader["versionminor"], shader["versionrevision"], shader["versionbuild"]))
         name = "V{}.{}.{}.{}_{}.upipelinecache".format(shader["versionmajor"], shader["versionminor"], shader["versionrevision"], shader["versionbuild"], index)
         index += 1
         data = base64.b64decode(shader["pipelinecachedata"])
--- tests/Push.py
+++ tests/Push.py
@@ -2,8 +2,11 @@ import requests
 import os
 import random
 import secrets
+import base64
+import json
 
-uploadURL = "/api/pco/new/rec/p/{}/m/{}/v/{}"
+# uploadURL = "/api/pco/new/rec/p/{}/m/{}/v/{}"
+uploadURL = "/api/pco/new/"
 
 machineID = secrets.token_hex(16)
 version = "0.0.1.1"
@@ -13,8 +16,25 @@ projid = "8ADEA8076EB5428A7CF34BFBCFFB656F53DC8C1872BF6D2558D57202D62B787E"
 
 uploadRun = uploadURL.format(projid, machineID, version)
 
-header = {"Content-type": "application/octet-stream"} 
+header = {"Content-type": "application/json"} 
 
+data = []
 with open("test.upipelinecache", "rb") as f:
-    p = requests.post("http://127.0.0.1:3000" + uploadRun, data=f.read(), headers=header)
+    data = f.read()
 
+jsonBody = {
+    "machine": machineID,
+    "project": projid,
+    "version": version,
+    "shadertype": "recorded",
+    "platform": "Direct",
+    "shadermodel": "SM5",
+    "data": base64.b64encode(data).decode('utf-8')
+}
+
+#print(base64.b64encode(data).decode('utf-8'))
+
+p = requests.post("http://127.0.0.1:3000" + uploadURL, data=json.dumps(jsonBody), headers=header)
+
+print(p.status_code)
+print(p.json())
